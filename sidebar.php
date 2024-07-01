876d06b31ecfa4a764a97c49a6f17a5acced54d9
Dicskson Owusu Nyantakyi <111504596+ondickson@users.noreply.github.com>
2024-07-01 12:33:25 +0800

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sidebar</title>
+  <link rel="stylesheet" href="./css/sidebar.css">
+</head>
+<body>
+<aside>
+  <nav>
+    <ul>
+      <li class="adminOption">
+        <h2>Admin</h2>
+        
+        <ul>
+          <li><a href="Admin_AddNew.php">Add New</a></li>
+          <li><a href="Admin_List.php">List All</a></li>
+        </ul>
+      </li>
+      <li class="adminOption">
+        <h2>Parcel</h2>
+        
+        <ul>
+          <li><a href="Parcel_AddNew.php">Add New</a></li>
+          <li><a href="Parcel_List.php">List All</a></li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
+</aside>
+</body>
+</html>
